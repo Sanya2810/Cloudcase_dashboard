44aadc02e2c0905488374355f88dff2f5de272f2
BF-L042\Sanya <sanya.anand28@gmail.com>
2022-11-30 16:36:46 +0100

    Added new packages to requirements.txt

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ def update_clear(n_clicks):
         return '', '', '', '','','','',''
 
 if __name__ == '__main__':
-    app.run_server(debug=True, dev_tools_props_check=False)
+    app.run_server(debug=False, dev_tools_props_check=False)
